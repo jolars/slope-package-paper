d9bdbff08c7f31bb797c7329a96a0768fb23f482
Johan Larsson <jolars@posteo.com>
2025-06-16 15:23:43 +0200

    chore: update pbs config files

--- pbs_config.sh
+++ pbs_config.sh
@@ -2,28 +2,30 @@
 
 SOLVERS=(
   "PGD[prox=prox_fast_stack,acceleration=fista]"
-  # "PGD[prox=prox_fast_stack,acceleration=bb]"
-  # "PGD[prox=prox_fast_stack,acceleration=anderson]"
-  "ADMM[adaptive_rho=True,rho=100]"
-  "rSLOPE"
+  "PGD[prox=prox_fast_stack,acceleration=bb]"
+  "PGD[prox=prox_fast_stack,acceleration=anderson]"
+  "ADMM[adaptive_rho=False,rho=100]"
   "sortedl1"
   "skglm"
   "SlopePath"
-  # "Newt-ALM"
+  "Newt-ALM[inner_solver=standard]"
   "PGD_safe_screening[accelerated=True]"
 )
 
 DATASETS=(
-  "Simulated[X_density=1.0,n_features=100,n_samples=2000,rho=0.5,n_signals=50]"
-  "Simulated[X_density=1.0,n_features=2000,n_samples=100,rho=0.5,n_signals=10]"
-  # "Simulated[X_density=0.001,n_features=200000,n_samples=2000,rho=0.0,n_signals=50]"
-  "breheny[dataset=bcTCGA,standardize=True]"
-  # "libsvm[dataset=YearPredictionMSD,standardize=True]"
-  # "breheny[dataset=Rhee200,standardize=True]"
+  "Simulated[X_density=1.0,n_features=20000,n_samples=100,rho=0.6,n_signals=20]"
+  "Simulated[X_density=1.0,n_features=100,n_samples=20000,rho=0.6,n_signals=40]"
+  "Simulated[X_density=0.001,n_features=200000,n_samples=2000,rho=0.6,n_signals=20]"
+  "breheny[dataset=brca1,standardize=True]"
+  "breheny[dataset=Rhee2006,standardize=True]"
+  "breheny[dataset=Koussounadis2014,standardize=True]"
+  "libsvm[dataset=rcv1.binary,standardize=True]"
+  "libsvm[dataset=real-sim,standardize=True]"
+  "libsvm[dataset=YearPredictionMSD,standardize=True]"
 )
 
 OBJECTIVES=(
-  "SLOPE[fit_intercept=True,q=0.2,reg=0.5]"
-  "SLOPE[fit_intercept=True,q=0.2,reg=0.1]"
-  # "SLOPE[fit_intercept=True,q=0.2,reg=0.01]"
+  "SLOPE[fit_intercept=False,q=0.2,reg=0.5]"
+  "SLOPE[fit_intercept=False,q=0.2,reg=0.1]"
+  "SLOPE[fit_intercept=False,q=0.2,reg=0.02]"
 )
